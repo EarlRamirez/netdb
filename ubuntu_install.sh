6c0a49f137a2dd855317826d6507bb4c404d85aa
Kay <kay.richter@googlemail.com>
2020-01-24 09:23:38 +0000

    first scribble of what to get as installer

---
+++ ubuntu_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#To make a more easy install on Ubuntu...
+
+#user creation
+
+
+
+#apt installs
+
+
+#perl crap (hope this can be automated)
+
+
+#linking stuff
+
+
+#init mysql
+
+
