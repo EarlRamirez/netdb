08b594cd2cb574d5df18caf2daf300a563482a12
kay <kay@netdb.fritz.box>
2020-02-14 17:45:50 +0000

    addded whitespaces and stuff

--- createdb.sql
+++ createdb.sql
@@ -1,3 +1,3 @@
-DROP DATABASE [IF EXISTS] database_name;
+DROP DATABASE IF EXISTS netdb;
 create database netdb;
 use netdb;
--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -2,6 +2,7 @@
 NETDBUSER="netdb"
 DATABASE="netdb"
 SQLSERVER="mysql-server"
+SQLCLIENTPACKAGE="mysql-client"
 SQLCLIENT="mysql"
 DBUSER="netdb"
 DBPASS="geheim"
@@ -21,15 +22,15 @@ if [ $retval -ne 0 ]; then
 fi
 
 #Installing mysql
-sudo apt install $SQLCLIENT $SQLSERVER
+sudo apt install $SQLCLIENTPACKAGE $SQLSERVER
 
 
 #apt installs
-apt update
+apt update &&
 apt-get install liblist-moreutils-perl libdbi-perl libnet-dns-perl libdatetime-perl \
-libdatetime-format-mysql-perl libossp-uuid-perl libdigest-hmac-perl libdigest-sha-perl libdigest-sha-perl\
+libdatetime-format-mysql-perl libossp-uuid-perl libdigest-hmac-perl libdigest-sha-perl libdigest-sha-perl \
 libwww-perl libexpect-perl libnet-telnet-cisco-perl libappconfig-perl libio-lockedfile-perl libnetaddr-ip-perl \
-libnet-ip-perl libio-socket-inet6-perl libfile-flock-perl libproc-processtable-perl
+libnet-ip-perl libio-socket-inet6-perl libfile-flock-perl libproc-processtable-perl liblist-moreutils-perl libappconfig-perl \
 
 apt-get install libyaml-perl unzip make bzip2 curl lynx ncftp ftp patch makepatch \
 libproc-queue-perl libnet-mac-vendor-perl libnet-openssh-perl
@@ -55,19 +56,25 @@ cp /opt/netdb/extra/netdb-logrotate /etc/logrotate.d/
 
 #init mysql
 echo "creating database"
-sudo SQLCLIENT < createdb.sql
-sudo SQLCLIENT netdb < createnetdb.sql
-sudo SQLCLIENT netdb < createuser.sql
+sudo $SQLCLIENT < createdb.sql
+sudo $SQLCLIENT netdb < createnetdb.sql
+sudo $SQLCLIENT netdb < createuser.sql
 #set user
-./extra/crontab >> /etc/crontab
-
+crontab -l >> file
+cat ./extra/crontab >> file
+crontab file
+rm file
 #cgi
-cp  /opt/netdb/netdb.cgi.conf /etc
-chmod a+r /etc/netdb.cgi.conf
+mkdir /var/www
+chown -R www-data:www-data /var/www/
+cp  /opt/netdb/netdb-cgi.conf /etc
+chmod a+r /etc/netdb-cgi.conf
 touch /var/www/netdbReport.csv
 chown www-data:www-data /var/www/netdbReport.csv
-cp netdb/extra/depends/ /var/www/depends
+cp -r /opt/netdb/extra/depends/ /var/www/depends
 chown --recursive  www-data:www-data  /var/www/depends
+mkdir /usr/cgi-bin
+chgrp -R www-data /usr/cgi-bin
 cp netdb.cgi.pl /usr/cgi-bin
 chown  www-data:www-data /usr/cgi-bin/netdb.cgi.pl
 #disbale user login
